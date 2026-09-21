e940d6421f15933529ce678a86634d1582754bfb
raji0903 <rajalakshmibabu09@gmail.com>
2026-09-21 22:24:13 +0530

    Time: 1 ms (99.34%) | Memory: 46.3 MB (50.95%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return buildTrees(1, n);
+    }
+
+    private List<TreeNode> buildTrees(int start, int end) {
+        List<TreeNode> allTrees = new ArrayList<>();
+
+        // Base case: no numbers to form a tree, return a list containing null
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // Pick each number i in [start, end] as the root
+        for (int i = start; i <= end; i++) {
+            // Generate all possible left and right subtrees
+            List<TreeNode> leftTrees = buildTrees(start, i - 1);
+            List<TreeNode> rightTrees = buildTrees(i + 1, end);
+
+            // Connect root i with all combinations of left and right subtrees
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    allTrees.add(root);
+                }
+            }
+        }
+
+        return allTrees;
+    }
+}
